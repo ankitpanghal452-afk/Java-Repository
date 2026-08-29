b64502367a8227f9b55e104f1d256a107b8df3a7
unknown <ankitpanghal452@gmail.com>
2026-08-29 22:31:44 +0530

    Learning Java

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Division = " + division);
+    }
+}
