1182ce742c54c412d6ef9c8f4c19d0ac9ae07dcb
unknown <ankitpanghal452@gmail.com>
2026-08-31 12:09:46 +0530

    Java

--- Main.java
+++ Main.java
@@ -1,10 +1,98 @@
-public class Main {
-    public static void main(String[] args) {
+// public class Main {
+//     public static void main(String[] args) {
 
-        int age = 20;
-        String name = "Ankit";
+//         int age = 20;
+//         String name = "Ankit";
 
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+//         System.out.println("Name: " + name);
+//         System.out.println("Age: " + age);
+//     }
+// }
+
+
+
+
+
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         int num=sc.nextInt();
+//         if(num==0){
+//             System.out.println("Number is Zero");
+//         }
+//         else if(num>0){
+//             System.out.println("Positive number");
+//         }
+//         else{
+//             System.out.println("Negative number");
+//         }
+//     }
+// }
+
+
+
+
+
+
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter the first number : ");
+//         int num=sc.nextInt();
+//         System.out.print("Enter the second number : ");
+//         int number=sc.nextInt();
+//         if(num==number){
+//             System.out.println("Both numbers are equal");
+//         }
+//         else if(num>number){
+//             System.out.println("First number is greater than Second number");
+//         }
+//         else{
+//             System.out.println("Second number is greater than first number");
+//         }
+//     }
+// }
+
+
+
+
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter the number : ");
+//         int num=sc.nextInt();
+//         if(num%2==0){
+//             System.out.println("Even number");
+//         }
+//         else{
+//             System.out.println("Odd number");
+//         }
+//     }
+// }
+
+
+
+
+
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number ");
+        int age=sc.nextInt();
+        if(age<0){
+            System.out.println("Invalid Age");
+        }
+        else if(age>18){
+            System.out.println("Eligible to vote");
+        }
+        else{
+            System.out.println("Not eligible to vote");
+        }
     }
 }
