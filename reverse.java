0496de529ed2abb12acf676356781aea509d30b1
ankitpanghal452-afk <ankitpanghal452@gmail.com>
2026-09-11 13:05:57 +0530

    It's Array traversal

--- reverse.java
+++ reverse.java
@@ -1,69 +1,111 @@
+// import java.util.Scanner;
+
+// public class reverse {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter length of an array : ");
+//         int length = sc.nextInt();
+
+//         int[] arr = new int[length];
+
+//         for (int i = 0; i < length; i++) {
+//             System.out.print("Enter element : ");
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter the number of element for which you want to rotate an array : ");
+//         int k = sc.nextInt();
+
+//         // In case k is greater than length
+//         k = k % length;
+
+//         // 1. Reverse whole array
+//         int left = 0;
+//         int right = length - 1;
+
+//         while (left < right) {
+//             int temp = arr[left];
+//             arr[left] = arr[right];
+//             arr[right] = temp;
+
+//             left++;
+//             right--;
+//         }
+
+//         // 2. Reverse first k elements
+//         left = 0;
+//         right = k - 1;
+
+//         while (left < right) {
+//             int temp = arr[left];
+//             arr[left] = arr[right];
+//             arr[right] = temp;
+
+//             left++;
+//             right--;
+//         }
+
+//         // 3. Reverse remaining elements
+//         left = k;
+//         right = length - 1;
+
+//         while (left < right) {
+//             int temp = arr[left];
+//             arr[left] = arr[right];
+//             arr[right] = temp;
+
+//             left++;
+//             right--;
+//         }
+
+//         // Print array
+//         for (int i = 0; i < length; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+// Move All Negative Numbers to One Side
 import java.util.Scanner;
-
-public class reverse {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
+public class reverse{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
         System.out.print("Enter length of an array : ");
-        int length = sc.nextInt();
-
-        int[] arr = new int[length];
-
-        for (int i = 0; i < length; i++) {
+        int length=sc.nextInt();
+        int[] arr=new int[length];
+        for(int i=0;i<length;i++){
             System.out.print("Enter element : ");
-            arr[i] = sc.nextInt();
+            arr[i]=sc.nextInt();
         }
-
-        System.out.print("Enter the number of element for which you want to rotate an array : ");
-        int k = sc.nextInt();
-
-        // In case k is greater than length
-        k = k % length;
-
-        // 1. Reverse whole array
-        int left = 0;
-        int right = length - 1;
-
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
-            left++;
-            right--;
+        int left=0;
+        int right=length-1;
+        while(left<right){
+            if(arr[left]>=0 && arr[right]<0){
+                int temp=arr[left];
+                arr[left]=arr[right];
+                arr[right]=temp;
+                left++;
+                right--;
+            }
+            else if(arr[left]<0){
+                left++;
+            }
+            else if(arr[left]>=0 && arr[right]>=0){
+                right--;
+            }
         }
-
-        // 2. Reverse first k elements
-        left = 0;
-        right = k - 1;
-
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
-            left++;
-            right--;
-        }
-
-        // 3. Reverse remaining elements
-        left = k;
-        right = length - 1;
-
-        while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
-            left++;
-            right--;
-        }
-
-        // Print array
-        for (int i = 0; i < length; i++) {
+        for(int i=0;i<length;i++){
             System.out.println(arr[i]);
         }
+        sc.close();
     }
 }
